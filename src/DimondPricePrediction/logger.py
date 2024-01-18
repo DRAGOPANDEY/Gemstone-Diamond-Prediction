ff399c6adb57f598f1ed5e6d85f9f4e431f7c8b0
Mohit Pandey <mohitpy6371@gmail.com>
2024-01-19 01:11:25 +0530

    update the code

---
+++ src/DimondPricePrediction/logger.py
@@ -0,0 +1,50 @@
+# import logging 
+# import os
+# from datetime import datetime
+
+# log_file=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# log_path=os.path.join(os.getcwd(),"logs")
+
+# os.makedirs(log_path,exist_ok=True)
+
+# log_filepath=os.path.join(log_path,log_file)
+
+
+# # logging.basicConfig(level=logging.INFO,
+# #                     filename=log_filepath,
+# #                     format ="[%(asctime)s] % (lineno)d %(name)s - %(levelname)s - %(messsage)s"
+
+# # )
+
+# logging.basicConfig(level=logging.INFO, 
+#                     filename=LOG_FILEPATH,
+#                     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
+                    
+# )
+
+# if __name__ == '__main__':
+#     logging.info("here again i am tesitng")
+
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+log_path=os.path.join(os.getcwd(),"logs")
+
+os.makedirs(log_path,exist_ok=True)
+
+LOG_FILEPATH=os.path.join(log_path,LOG_FILE)
+
+
+logging.basicConfig(level=logging.INFO, 
+                    filename=LOG_FILEPATH,
+                    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
+                    
+)
+
+
+if __name__ == '__main__':
+    logging.info("here again i am tesitng")
